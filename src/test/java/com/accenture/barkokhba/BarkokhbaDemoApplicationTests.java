ccdc3611fffca0e7304cb60d34650e90e09036d7
andras.toth <andras.toth@accenture.com>
2019-01-31 11:33:28 +0100

    Initial commit

---
+++ src/test/java/com/accenture/barkokhba/BarkokhbaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.accenture.barkokhba;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BarkokhbaDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
